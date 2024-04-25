ce97732f74375ce6fb54870a4d184cba83c2f163
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2024-04-26 01:13:27 +0530

    Handled session

--- chatbot.py
+++ chatbot.py
@@ -5,12 +5,10 @@ from model_processor import MedicalModel
 
 chatbot = Blueprint('chatbot', __name__)
 
-
-
 @chatbot.route('/chatbot',  methods=['POST'])
 def promptInput():
-    argument = request.args
-    if userauth.verify_string(argument.get("email"), argument.get("salt"), argument.get("hash_string")):
+    argument = request.headers
+    if userauth.validate_from_database(argument.get("email"), argument.get("salt"), argument.get("hash_string")):
         model = MedicalModel(dict(request.json).get("message"))
         return model.give_tips()
     else:
--- userauth.py
+++ userauth.py
@@ -3,6 +3,8 @@ from flask import request
 from db_connector import DbConnector
 import hashlib
 import secrets
+import traceback
+
 
 
 auth = Blueprint('user_auth', __name__)
@@ -18,28 +20,34 @@ def register():
 def generate_salt():
     return secrets.token_hex(16)
 
-def hash_string(string, salt):
-    salted_string = string.encode() + salt.encode()
-    hashed_string = hashlib.sha256(salted_string).hexdigest()
-    return hashed_string
 
 def store_salted_string(email, k):
     salt = generate_salt()
     hashed_string = hash_string(email, salt)
     try:
-        if k.get_records('session', {"email" : email}) is not None:
-            k.update_record('session',  {"email":email}, {'email': email, 'salt': salt, 'hashed_string': hashed_string})
+        if len(k.get_records('session', {"email" : email} )) > 0:
+            print("Update called")
+            k.delete_record('session', {"email":email})
+            k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
+        else:
+            print("Insert called")
+            k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
     except:
+        traceback.print_exc()
+        print("Insert called")
         k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
-    
     return {
         'email': email,
         'salt': salt,
         'hashed_string': hashed_string
     }
 
-def verify_string(email, salt, hashed_string):
-    if hash_string(email, salt) == hashed_string:
+def validate_from_database(email, salt, hash_string):
+    k = DbConnector.get_instance()
+    data = k.get_records('session', {'email' : email})
+    print()
+    if(data[0].get('email') == str(email) and data[0].get('salt') == salt and data[0].get('hash_string') == hash_string):
+        print(email)
         return True
     else:
         return False
@@ -49,7 +57,8 @@ def login():
     login_data = request.json
     k = DbConnector.get_instance()
     data = k.get_records('dc_userpool', {"email" : login_data.get("email")})
-    if (data is not None):
+    print(data)
+    if (len(data)>0):
         return store_salted_string(email=login_data.get('email'),k=k)
     else:
         return {"error":"Invalid User"}
