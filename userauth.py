3ec300a07c1ebb686d83f54e8b33e4efdf29ade3
Hari-Nikesh-R <hari.nikesh.r.cce@gmail.com>
2024-04-26 09:19:47 +0530

    Handled session and added logout endpoint

--- userauth.py
+++ userauth.py
@@ -23,44 +23,46 @@ def generate_salt():
 
 def store_salted_string(email, k):
     salt = generate_salt()
-    hashed_string = hash_string(email, salt)
     try:
         if len(k.get_records('session', {"email" : email} )) > 0:
             print("Update called")
             k.delete_record('session', {"email":email})
-            k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
+            k.insert_data('session', {'email': email, 'salt': salt})
         else:
             print("Insert called")
-            k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
+            k.insert_data('session', {'email': email, 'salt': salt})
     except:
         traceback.print_exc()
         print("Insert called")
-        k.insert_data('session', {'email': email, 'salt': salt, 'hashed_string': hashed_string})
+        k.insert_data('session', {'email': email, 'salt': salt})
     return {
         'email': email,
-        'salt': salt,
-        'hashed_string': hashed_string
+        'salt': salt
     }
 
 def validate_from_database(email, salt, hash_string):
     k = DbConnector.get_instance()
     data = k.get_records('session', {'email' : email})
-    print()
-    if(data[0].get('email') == str(email) and data[0].get('salt') == salt and data[0].get('hash_string') == hash_string):
-        print(email)
-        return True
-    else:
+    try:
+        if(data[0].get('email') == str(email) and data[0].get('salt') == salt):
+            print(email)
+            return True
+        else:
+            return False
+    except:
         return False
 
 @auth.route('/login', methods=['POST'])
 def login():
     login_data = request.json
     k = DbConnector.get_instance()
+    print("email" + str(login_data.get("email")))
     data = k.get_records('dc_userpool', {"email" : login_data.get("email")})
     print(data)
     if (len(data)>0):
         return store_salted_string(email=login_data.get('email'),k=k)
     else:
+        print("Invalid User")
         return {"error":"Invalid User"}
 
 @auth.route('/update',methods=['POST'])
@@ -78,6 +80,16 @@ def delete():
     k=DbConnector.get_instance()
     k.delete_record(collection_name='dc_userpool',request_data=delete_data)
     return "Deleted Successfully"
+
+@auth.route('/logout', methods=['GET'])
+def logout():
+    email = request.headers.get('email')
+    k = DbConnector.get_instance()
+    k.delete_record(collection_name='session', request_data= {"email": email})
+    return {
+        "result":"Session cleared",
+        "success":True
+    }
 
 
 @auth.route('/get_records',methods=['GET'])
